7dd52c374444b2ee31b1614c35a67d9f8c641689
sarthakdarade07 <sarthakdarade07@gmail.com>
2025-06-09 11:43:26 +0530

    added heading , dropdown in playlist section

--- src/Components/HomeContent.js
+++ src/Components/HomeContent.js
@@ -1,5 +1,6 @@
 import styles from "./HomeContent.module.css";
-import NavBar from "./NavBar";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 function HomeContent() {
   console.log("hi");
   return (
@@ -21,13 +22,71 @@ function HomeContent() {
                 </button>
               </h6>
             </div>
+            <div className={styles.dropdownContainer}>
+              <button type="button" class="btn" id={styles.playListBtn}>
+                Playlist
+              </button>
+              <h6 id={styles.sortIndicator}>Recents</h6>
+              <div class="dropdown">
+                <button
+                  class="btn btn-secondary"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  id={styles.dropdownButton}
+                >
+                  <i class="bi bi-list-ul"></i>
+                </button>
+                <ul
+                  class="dropdown-menu dropdown-menu-dark"
+                  id={styles.dropdownlist}
+                >
+                  <li>
+                    <p class="dropdown-item " id={styles.liText}>
+                      Sort
+                    </p>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Recents
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Recently Added
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Alphabetical
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Creator
+                    </a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <p class="dropdown-item" id={styles.liText}>
+                      view as
+                    </p>
+                  </li>
+                  <li>
+                    <div id={styles.viewasContainer}>hi</div>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
         <div className={styles.columns} id={styles.column2}>
           <div className={styles.content}></div>
         </div>
         <div className={styles.columns} id={styles.column3}>
-          <div className={styles.content}></div> 
+          <div className={styles.content}></div>
         </div>
       </div>
     </>

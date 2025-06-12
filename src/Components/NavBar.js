53fd788ad60690cb0cb3f2abf31c199f10b7fe6c
sarthakdarade07 <sarthakdarade07@gmail.com>
2025-06-12 22:43:03 +0530

    set a flag to manage click events of download appa and explore premium

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,169 +3,87 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import styles from "./NavBar.module.css";
+import { use, useEffect, useState } from "react";
+import RecommondedSongs from "./RecommondedSongs";
+import HomeDisplay from "./HomeDisplay";
+import HomeContent from "./HomeContent";
 
-function NavBar() {
+function NavBar(props) {
+  let [exPremimumFlag, setExPremimumFlag] = useState(false);
+  
+  function funClickExPremimum(){
+    setExPremimumFlag(!exPremimumFlag);
+    props.sendExPremimumFlag(exPremimumFlag);
+  }
+  useEffect(()=>{
+   return ()=>{
+    setExPremimumFlag(false);
+   }
+  },props)
   return (
     <>
-      <>
-        <Navbar
-          expand="lg"
-          id={styles.nav}
-          data-bs-theme="dark"
-          breakpoints="sm"
-        >
-          <a
-            class="navbar-brand"
-            href="#"
-            data-toggle="tooltip"
-            data-placement="bottom"
-            title="Spotify"
-          >
-            <i
-              class="bi bi-spotify text-white fs-1"
-              id={styles.spotifyIcon}
-            ></i>
-          </a>
-          <Container fluid>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: "100px" }}
-                navbarScrol
-              >
-                <button
-                  type="button"
-                  class="btn btn-light"
-                  id={styles.homeIcon}
-                  data-toggle="tooltip"
-                  data-placement="bottom"
-                  title="Home"
-                >
-                  <i class="bi bi-house-door-fill text-white "></i>
+    <Navbar expand="lg" id={styles.nav} data-bs-theme="dark" breakpoints="sm">
+      <a className="navbar-brand" href="#" data-toggle="tooltip" data-placement="bottom" title="Spotify">
+        <i className="bi bi-spotify text-white fs-1" id={styles.spotifyIcon}></i>
+      </a>
+      <Container fluid>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
+            <button type="button" className="btn btn-light" id={styles.homeIcon} data-toggle="tooltip" data-placement="bottom" title="Home">
+              <i className="bi bi-house-door-fill text-white"></i>
+            </button>
+
+            <Form className="d-flex">
+              <i className="bi bi-search" id={styles.serch_icon}></i>
+              <Form.Control type="search" placeholder="What do you want to play?" className="me-2" aria-label="Search" id={styles.nav_search} />
+            </Form>
+
+            <Nav.Link href="#">
+              <span id={styles.browseIcon}> | <i className="bi bi-archive"></i></span>
+            </Nav.Link>
+
+           
+              <button id={styles.nav_button} type="button" className="btn btn-light" data-toggle="tooltip" data-placement="bottom" title="Upgrade to Premium"
+               onClick={funClickExPremimum}>
+                Explore Premium
+              </button>
+
+              <Nav.Link href="#" id={styles.spanTxt} data-toggle="tooltip" data-placement="bottom" title="Install Now">
+                <i className="bi bi-file-arrow-down"></i><span>Install App</span>
+              </Nav.Link>
+
+              <Nav.Link href="#" id={styles.icon} data-toggle="tooltip" data-placement="bottom" title="Whats's New">
+                <i className="bi bi-bell text-white"></i>
+              </Nav.Link>
+
+              <Nav.Link href="#" id={styles.icon} data-toggle="tooltip" data-placement="bottom" title="Friend Activity">
+                <i className="bi bi-person text-white"></i><i className="bi bi-person text-white"></i>
+              </Nav.Link>
+
+              <div className="dropdown" data-toggle="tooltip" data-placement="bottom" title="Sarthak Darade" id={styles.profileIcon}>
+                <button className="btn btn-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i className="bi bi-person-circle"></i>
                 </button>
-
-                <Form className="d-flex">
-                  <i class="bi bi-search" id={styles.serch_icon}></i>
-                  <Form.Control
-                    type="search"
-                    placeholder="What do you want to play?"
-                    className="me-2"
-                    aria-label="Search"
-                    id={styles.nav_search}
-                  />
-                </Form>
-                <Nav.Link href="#">
-                  <span id={styles.browseIcon}>
-                    {" "}
-                    | <i class="bi bi-archive"></i>
-                  </span>
-                </Nav.Link>
-                <button
-                  id={styles.nav_button}
-                  type="button"
-                  class="btn btn-light"
-                  data-toggle="tooltip"
-                  data-placement="bottom"
-                  title="Upgrade to Premium"
-                >
-                  Explore Premium
-                </button>
-
-                <Nav.Link
-                  href="#"
-                  id={styles.spanTxt}
-                  data-toggle="tooltip"
-                  data-placement="bottom"
-                  title="Install Now"
-                >
-                  <i class="bi bi-file-arrow-down"></i>
-                  <span>Install App</span>
-                </Nav.Link>
-
-                <Nav.Link
-                  href="#"
-                  id={styles.icon}
-                  data-toggle="tooltip"
-                  data-placement="bottom"
-                  title="Whats's New"
-                >
-                  <i class="bi bi-bell text-white "></i>
-                </Nav.Link>
-
-                <Nav.Link
-                  href="#"
-                  id={styles.icon}
-                  data-toggle="tooltip"
-                  data-placement="bottom"
-                  title="Friend Activity"
-                >
-                  <i class="bi bi-person  text-white"></i>
-                  <i class="bi bi-person  text-white"></i>
-                </Nav.Link>
-
-                <div
-                  class="dropdown"
-                  data-toggle="tooltip"
-                  data-placement="bottom"
-                  title="Sarthak Darade"
-                  id={styles.profileIcon}
-                >
-                  <button
-                    class="btn btn-secondary"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    <i class="bi bi-person-circle"></i>
-                  </button>
-                  <ul
-                    class="dropdown-menu dropdown-menu-dark dropdown-menu-lg-end"
-                    id={styles.userIconDropdown}
-                  >
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Account
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Profile
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Upgrade to Premium
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Support
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Download
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Settings
-                      </a>
-                    </li>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">
-                      Log Out
-                    </a>
-                  </ul>
-                </div>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+                <ul className="dropdown-menu dropdown-menu-dark dropdown-menu-lg-end" id={styles.userIconDropdown}>
+                  <li><a className="dropdown-item" href="#">Account</a></li>
+                  <li><a className="dropdown-item" href="#">Profile</a></li>
+                  <li><a className="dropdown-item" href="#">Upgrade to Premium</a></li>
+                  <li><a className="dropdown-item" href="#">Support</a></li>
+                  <li><a className="dropdown-item" href="#">Download</a></li>
+                  <li><a className="dropdown-item" href="#">Settings</a></li>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item" href="#">Log Out</a>
+                </ul>
+              </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      
       </>
-    </>
   );
+
 }
 
 export default NavBar;

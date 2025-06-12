897172d766375e8aa4c2b63cc75c1196a9a490f7
sarthakdarade07 <sarthakdarade07@gmail.com>
2025-06-12 22:59:52 +0530

    added plan cards

--- src/Components/ExplorePremium.js
+++ src/Components/ExplorePremium.js
@@ -117,6 +117,55 @@ function ExplorePremium(){
               </button>
             </div>
           </div>
+
+          {/*----------------------------------------- card 3---------------------------------------- */}
+          <div class="card text-bg-dark mb-3" style={{ maxWidth: "18rem" }}>
+            <div
+              class="card-header"
+              style={{
+                backgroundColor: "#FFC862",
+                width: "50%",
+                fontSize: "medium",
+                borderRadius: "5%",
+              }}>
+              <span style={{ color: "black" }}>
+                {" "}
+                <i class="fa-solid fa-indian-rupee-sign"></i> 149 for 2 months{" "}
+              </span>
+            </div>
+            <div class="card-body">
+              <h2>Family</h2>
+              <h5 class="card-title">
+                <i className="bi bi-spotify text-white fs-1"></i>
+                <span style={{ fontSize: "x-large", color: "#FFC862" }}>
+                  {" "}
+                  Duo{" "}
+                </span>
+              </h5>
+              <p class="card-text">
+                <i class="fa-solid fa-indian-rupee-sign"></i> 149 for 2 months{" "}
+                <br />
+                <i class="fa-solid fa-indian-rupee-sign"></i> 149/month after
+                <hr></hr>
+                <ul>
+                  <li>2 Premium account</li>
+                  <li>Cancel anytime</li>
+                  <li>Suscribe or one-time payment</li>
+                </ul>
+              </p>
+              <button
+                type="button"
+                class="btn btn-light"
+                style={{
+                  marginLeft: "10%",
+                  backgroundColor: "#FFC862",
+                  borderColor: " #FFC862",
+                }}>
+                Try 2 months for <i class="fa-solid fa-indian-rupee-sign"></i>{" "}
+                149
+              </button>
+            </div>
+          </div>
         </div>
       </>
     );

fc71bbeb5ce29b1f8d2eb9ab132b11096ecf38a2
sarthakdarade07 <sarthakdarade07@gmail.com>
2025-06-20 18:23:10 +0530

    Added Alert Component it will generate popup for add to fav,remove from fav and not song avilable.

---
+++ src/Components/Alerts.js
@@ -0,0 +1,41 @@
+import { Modal } from "react-bootstrap"
+import styles from "./Alerts.module.css"
+import { useEffect, useState } from "react";
+
+export default function  Alerts(props){
+   
+
+    let [show,setShow]=useState(true);
+    let [modalStyle,setModalstyle]=useState();
+
+    useEffect(()=>{
+        setShow(true);
+        setTimeout(() => {
+          setShow(false);
+        }, 1200);
+
+      setModalstyle({
+        backgroundColor:props.popUpObj.bgColor,
+      })
+        
+    },[props])
+   
+
+
+
+    return (
+      <>
+        {modalStyle && (
+          <Modal show={show} className={styles.modal}>
+            <Modal.Body className={styles.modalBody} style={modalStyle}>
+              <p>
+                <i className={props.popUpObj.icon}></i>
+                {props.popUpObj.message}
+              </p>
+            </Modal.Body>
+          </Modal>
+        )}
+      </>
+    );
+
+}

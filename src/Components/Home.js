541f90a7c70f9f967e7cac83cbfc69f4fa012ff8
sarthakdarade07 <sarthakdarade07@gmail.com>
2025-06-20 18:24:52 +0530

    After getting popup details it will render alert compnent

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,13 +3,22 @@ import ApiCalls from "./ApiCalls";
 import NavBar from "./NavBar";
 import HomeContent from "./HomeContent";
 import FooterMusicContoller from "./FooterMusicController";
+import Alerts from "./Alerts";
 
 function Home(props) {
   let [playingSong, setPlayingSong] = useState();
+  //popUpObj from Tracks to HomeContent to Home
+  const [popUpObj, setPopUpObj] = useState();
+  const [popUpFlag,setPopUpFlag]=useState();
 
+  function funSetPopUp(obj){
+    setPopUpObj(obj);
+    setPopUpFlag(true);
+  }
   useEffect(() => {
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white"; // optional
+    setPopUpFlag(false);
   }, [props, playingSong]);
 
 
@@ -25,10 +34,12 @@ function Home(props) {
           {
             setPlayingSong(obj);
           }
-        }}>
-        {" "}
+        }}
+        getPopUpObj={funSetPopUp}>
       </HomeContent>
       {playingSong && <FooterMusicContoller playingSong={playingSong} />}
+
+      {popUpFlag && <Alerts popUpObj={popUpObj} />}
     </>
   );
 }
